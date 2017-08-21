de7cdbad5fd65ca22570200a4953487f39d1bc15
Nathan-Williams1 <nathan.williams@Inmarsat.com>
2017-08-21 13:59:41 -0230

    Sonar Cube Pacifacation

--- src/main/java/com/inmarsat/esb/client/SAPExchangeClient.java
+++ src/main/java/com/inmarsat/esb/client/SAPExchangeClient.java
@@ -1,19 +1,16 @@
 package com.inmarsat.esb.client;
 
-import com.inmarsat.esb.client.EXCHANGERATE01;
 import com.inmarsat.esb.client.EXCHANGERATEREQUEST;
-import com.inmarsat.esb.client.EXCHANGERATERESPONSE;
-import com.inmarsat.esb.client.HEADERINFO;
-import com.inmarsat.esb.client.ObjectFactory;
+
 import com.inmarsat.esb.client.SAPExchangeService;
 import com.inmarsat.esb.client.SAPExchangeServiceEndpoint;
 
 public class SAPExchangeClient {
 	public static void main(String[] args) {
-	SAPExchangeService sapExchangeService = new SAPExchangeService();
-	SAPExchangeServiceEndpoint sapExchangeServiceEnpoint;
-	sapExchangeServiceEnpoint = sapExchangeService.getSAPExchangeServicePort();
-	EXCHANGERATEREQUEST exchangeRateRequest = new EXCHANGERATEREQUEST();
-	sapExchangeServiceEnpoint.processSAPExchangeRate(exchangeRateRequest);
+		SAPExchangeService sapExchangeService = new SAPExchangeService();
+		SAPExchangeServiceEndpoint sapExchangeServiceEnpoint;
+		sapExchangeServiceEnpoint = sapExchangeService.getSAPExchangeServicePort();
+		EXCHANGERATEREQUEST exchangeRateRequest = new EXCHANGERATEREQUEST();
+		sapExchangeServiceEnpoint.processSAPExchangeRate(exchangeRateRequest);
 	}
 }
