7b56eb88f7843802a554eda47b2f7e07670fd022
Nathan-Williams1 <nathan.williams@Inmarsat.com>
2017-08-21 16:23:52 -0230

    SOAP request added

--- src/main/java/com/inmarsat/esb/App.java
+++ src/main/java/com/inmarsat/esb/App.java
@@ -1,6 +1,6 @@
 package com.inmarsat.esb;
 
-
 public class App {
-
+	public static void main(String[] args){
+	}
 }
--- src/test/java/com/inmarsat/esb/BehaviourTest.java
+++ src/test/java/com/inmarsat/esb/BehaviourTest.java
@@ -9,6 +9,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -22,6 +27,15 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.unix4j.Unix4j;
 
+import com.inmarsat.esb.client.EXCHANGERATE01;
+import com.inmarsat.esb.client.EXCHANGERATEREQUEST;
+import com.inmarsat.esb.client.HEADERINFO;
+import com.inmarsat.esb.client.SAPExchangeService;
+import com.inmarsat.esb.client.SAPExchangeServiceEndpoint;
+import com.inmarsat.esb.client.EXCHANGERATE01.IDOC;
+import com.inmarsat.esb.client.EXCHANGERATE01.IDOC.E1EXCHANGERATE;
+import com.inmarsat.esb.client.EXCHANGERATE01.IDOC.EDIDC40;
+
 public class BehaviourTest
 {
   private static final Logger logger = Logger.getLogger(BehaviourTest.class);
@@ -30,8 +44,9 @@ public class BehaviourTest
   public void bodyTest()
   {
 	BasicConfigurator.configure();
-	//post();
-	//delete();
+	delete();
+	post();
+	soap();
     get();
     File file = new File("src/test/resources/response.txt");
     String name = Unix4j.grep("Name", new File[] { file }).toStringResult();
@@ -155,5 +170,84 @@ public class BehaviourTest
       logger.error("IO Exception", e);
       e.printStackTrace();
     }
+  }
+  public static void soap() {
+	  SAPExchangeService sapExchangeService = new SAPExchangeService();
+		SAPExchangeServiceEndpoint sapExchangeServiceEndpoint;
+		sapExchangeServiceEndpoint = sapExchangeService.getSAPExchangeServicePort();
+		EXCHANGERATEREQUEST exchangeRateRequest = new EXCHANGERATEREQUEST();
+		HEADERINFO headerinfo = new HEADERINFO();
+		headerinfo.setORIGINALSYSTEM("SAP");
+		XMLGregorianCalendar calendar;
+		try {
+			calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar();
+			calendar.setYear(2001);
+			calendar.setMonth(10);
+			calendar.setDay(26);
+			calendar.setTime(21, 32, 52);
+			headerinfo.setORIGINALSTYSTEMDATETIME(calendar);
+			headerinfo.setSENDERSYSTEMDATETIME(calendar);
+		} catch (DatatypeConfigurationException e) {
+			logger.error("Datatype Configuration Exception", e);
+		}
+		headerinfo.setSENDERSYSTEM("SAP");
+		headerinfo.setSPAREATTR1("att1");
+		headerinfo.setSPAREATTR2("att2");
+		headerinfo.setSPAREATTR3("att3");
+		headerinfo.setSPAREATTR4("att4");
+		headerinfo.setSPAREATTR5("att5");
+		exchangeRateRequest.setHEADERINFO(headerinfo);
+		EXCHANGERATE01 exchangeRate01 = new EXCHANGERATE01();
+		IDOC idoc = new IDOC();
+		idoc.setBEGIN("1");
+		EDIDC40 edidc40 = new EDIDC40();
+		edidc40.setSEGMENT("1");
+		edidc40.setTABNAM("EDI_DC40");
+		edidc40.setMANDT("200");
+		edidc40.setDOCNUM("0000000004389344");
+		edidc40.setDOCREL("740");
+		edidc40.setSTATUS("03");
+		edidc40.setDIRECT("1");
+		edidc40.setOUTMOD("2");
+		edidc40.setEXPRSS("0");
+		edidc40.setTEST("1");
+		edidc40.setIDOCTYP("EXCHANGE_RATE01");
+		edidc40.setCIMTYP("");
+		edidc40.setMESTYP("EXCHANGE_RATE");
+		edidc40.setMESCOD("mes");
+		edidc40.setMESFCT("mes");
+		edidc40.setSTD("1");
+		edidc40.setSTDVRS("stdvrs");
+		edidc40.setSTDMES("stdmes");
+		edidc40.setSNDPOR("SAPDE1");
+		edidc40.setSNDPRT("LS");
+		edidc40.setSNDPFC("sn");
+		edidc40.setSNDPRN("DE1CLNT200");
+		edidc40.setSNDSAD("sndsad");
+		edidc40.setSNDLAD("sndlad");
+		edidc40.setRCVPOR("SAPDE1");
+		edidc40.setRCVPRT("LS");
+		edidc40.setRCVPFC("r");
+		edidc40.setRCVPRN("EXECCPISV");
+		edidc40.setRCVSAD("rcvsad");
+		edidc40.setRCVLAD("rcvlad");
+		edidc40.setCREDAT("20161211");
+		edidc40.setCRETIM("133306");
+		edidc40.setREFINT("refint");
+		edidc40.setREFGRP("refgrp");
+		edidc40.setREFMES("refmes");
+		edidc40.setARCKEY("arckey");
+		edidc40.setSERIAL("20161211133306");
+		idoc.setEDIDC40(edidc40);
+		E1EXCHANGERATE e1ExchangeRate = new E1EXCHANGERATE();
+		e1ExchangeRate.setSEGMENT("1");
+		e1ExchangeRate.setLOGSYSTEM("DE1CLNT200");
+		e1ExchangeRate.setUPDALLOW("0");
+		e1ExchangeRate.setCHGFIXED("1");
+		e1ExchangeRate.setDEVALLOW("de1");
+		idoc.setE1EXCHANGERATE(e1ExchangeRate);
+		exchangeRate01.setIDOC(idoc);
+		exchangeRateRequest.setEXCHANGERATE01(exchangeRate01);
+		sapExchangeServiceEndpoint.processSAPExchangeRate(exchangeRateRequest);
   }
 }
